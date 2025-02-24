d87208534f24c6e4490c5423dca47ac6850a4e53
Tomas Pesek <pesekt@gmail.com>
2025-02-24 09:30:52 +0100

    added images to GenreList

--- rawg-client/src/components/GenreList.tsx
+++ rawg-client/src/components/GenreList.tsx
@@ -1,18 +1,29 @@
+import { Heading, HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
+import getCroppedImageUrl from "../services/image-url";
 
 const GenreList = () => {
-  const { data: genres, error, isLoading } = useGenres();
+  const { data: genres } = useGenres();
 
   return (
-    <div>
-      {error && <p>{error}</p>}
-      {isLoading && <p>Loading...</p>}
-      <ul>
+    <>
+      <Heading>Genres</Heading>
+      <List>
         {genres.map((genre) => (
-          <li key={genre.id}>{genre.name}</li>
+          <ListItem key={genre.id} paddingY="5px">
+            <HStack>
+              <Image
+                src={getCroppedImageUrl(genre.image_background)}
+                boxSize="32px"
+                borderRadius={8}
+                objectFit="cover"
+              />
+              <Text>{genre.name}</Text>
+            </HStack>
+          </ListItem>
         ))}
-      </ul>
-    </div>
+      </List>
+    </>
   );
 };
 
