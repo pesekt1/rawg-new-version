a0b506b95fb3b8bb0b2b3be951fa267cf2165b22
Tomas Pesek <pesekt@gmail.com>
2025-04-24 00:11:20 +0200

    edit icon

--- rawg-client/src/components/CustomList.tsx
+++ rawg-client/src/components/CustomList.tsx
@@ -12,6 +12,7 @@ import getCroppedImageUrl from "../services/image-url";
 import { useState } from "react";
 import { UseQueryResult } from "@tanstack/react-query";
 import { Response } from "../services/api-client";
+import { FaEdit } from "react-icons/fa";
 
 interface Props<T> {
   title: string;
@@ -24,6 +25,11 @@ interface Item {
   id: number;
   image_background: string;
   name: string;
+}
+
+// Helper to check admin login (simple version, adapt as needed)
+function isAdminLoggedIn() {
+  return !!localStorage.getItem("token");
 }
 
 const CustomList = <T extends Item>({
@@ -46,24 +52,37 @@ const CustomList = <T extends Item>({
 
   return (
     <Box marginBottom="6">
-      <Button
-        variant="link"
-        onClick={() => onSelectedItemId(undefined)}
-        bg="transparent"
-        _hover={{
-          textDecoration: "none",
-          color: colorMode === "light" ? "accent.600" : "white",
-        }}
-        fontSize="2xl"
-        fontWeight="bold"
-        color={colorMode === "light" ? "gray.800" : "white"}
-        _active={{
-          color: colorMode === "light" ? "accent.700" : "yellow.300",
-          bg: colorMode === "light" ? "lightGray.300" : "accent.500",
-        }}
-      >
-        <Heading size="lg">{title}</Heading>
-      </Button>
+      <HStack>
+        <Button
+          variant="link"
+          onClick={() => onSelectedItemId(undefined)}
+          bg="transparent"
+          _hover={{
+            textDecoration: "none",
+            color: colorMode === "light" ? "accent.600" : "white",
+          }}
+          fontSize="2xl"
+          fontWeight="bold"
+          color={colorMode === "light" ? "gray.800" : "white"}
+          _active={{
+            color: colorMode === "light" ? "accent.700" : "yellow.300",
+            bg: colorMode === "light" ? "lightGray.300" : "accent.500",
+          }}
+        >
+          <Heading size="lg">{title}</Heading>
+        </Button>
+        {/* Show admin update icon only if admin is logged in */}
+        {isAdminLoggedIn() && (
+          <Box
+            as={FaEdit}
+            boxSize={5}
+            color={colorMode === "light" ? "gray.600" : "yellow.300"}
+            cursor="pointer"
+            title="Update"
+            // onClick handler can be added here for admin update functionality
+          />
+        )}
+      </HStack>
       <List>
         {displayedItems?.map((item) => (
           <ListItem key={item.id} paddingY="5px">
