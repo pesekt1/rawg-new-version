1f3a513c0b08c56a8770e57096e7ca7d309ca71b
Tomas Pesek <pesekt@gmail.com>
2025-04-28 23:40:34 +0200

    made cropping function more generic

--- rawg-client/src/utils/image-url.ts
+++ rawg-client/src/utils/image-url.ts
@@ -2,7 +2,7 @@ import noImagePlaceholder from "../assets/no-image-placeholder-6f3882e0.webp";
 
 const getCroppedImageUrl = (url: string | null) => {
   if (!url) return noImagePlaceholder;
-  return url.replace("media/games", "media/crop/600/400/games");
+  return url.replace("/media/", "/media/crop/600/400/");
 };
 
 export default getCroppedImageUrl;
