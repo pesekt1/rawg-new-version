9c2f2dda17f5fa932dc30351e92e98940435ca23
Tomas Pesek <pesekt@gmail.com>
2025-04-30 00:10:18 +0200

    added fetchFirstTrailer method

--- rawg-client/src/domains/trailers/trailerService.ts
+++ rawg-client/src/domains/trailers/trailerService.ts
@@ -4,4 +4,10 @@ import { Trailer } from "./Trailer";
 const trailerService = (gameId: number) =>
   new ApiClient<Trailer>(`/games/${gameId}/movies`);
 
+export const fetchFirstTrailer = async (gameId: number) => {
+  // Assumes the API supports page_size param
+  const res = await trailerService(gameId).getAll({ params: { page_size: 1 } });
+  return res.results[0];
+};
+
 export default trailerService;
