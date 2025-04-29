f1e9c30cc365a10fc09cffb3c0ec2c0aecb3dec4
Tomas Pesek <pesekt@gmail.com>
2025-04-29 23:25:22 +0200

    added enabled to be able to cache conditionally, also allowed numbers in the query key

--- rawg-client/src/hooks/useGetEntities.ts
+++ rawg-client/src/hooks/useGetEntities.ts
@@ -2,14 +2,25 @@ import { useQuery, UseQueryOptions } from "@tanstack/react-query";
 import ms from "ms";
 
 interface UseGetEntitiesParams<T> {
-  queryKey: string[];
+  queryKey: (string | number)[];
   queryFn: () => Promise<T>;
   placeholderData?: T;
   staleTime?: number;
   cacheTime?: number;
+  /**
+   * enabled: Controls whether the query should automatically run.
+   * Useful for dependent queries or conditional fetching.
+   * Defaults to true.
+   */
+  enabled?: boolean;
   options?: Omit<
     UseQueryOptions<T, Error>,
-    "queryKey" | "queryFn" | "placeholderData" | "staleTime" | "cacheTime"
+    | "queryKey"
+    | "queryFn"
+    | "placeholderData"
+    | "staleTime"
+    | "cacheTime"
+    | "enabled"
   >;
 }
 
@@ -19,6 +30,7 @@ const useGetEntities = <T>({
   placeholderData,
   staleTime = ms("1d"),
   cacheTime = ms("1d"),
+  enabled = true,
   options = {},
 }: UseGetEntitiesParams<T>) =>
   useQuery<T, Error>({
@@ -27,6 +39,7 @@ const useGetEntities = <T>({
     placeholderData,
     staleTime,
     cacheTime,
+    enabled, //controls if the query should run
     ...options,
   });
 
