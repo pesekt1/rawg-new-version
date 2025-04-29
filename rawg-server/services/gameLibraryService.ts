5c153be055f6bb45b528b0fa6068aac3fcc20454
Tomas Pesek <pesekt@gmail.com>
2025-04-29 16:51:52 +0200

    finalized the implementation of wishlist and library services

--- rawg-server/services/gameLibraryService.ts
+++ rawg-server/services/gameLibraryService.ts
@@ -1,15 +1,3 @@
 import { UserCollectionService } from "./userCollectionService";
 
-const libraryService = new UserCollectionService("library");
-
-export const getUserWithLibrary = async (userId: number) => {
-  return libraryService.getUserWithCollection(userId);
-};
-
-export const addToLibrary = async (userId: number, gameId: number) => {
-  return libraryService.addToCollection(userId, gameId);
-};
-
-export const removeFromLibrary = async (userId: number, gameId: number) => {
-  return libraryService.removeFromCollection(userId, gameId);
-};
+export const gameLibraryService = new UserCollectionService("library");
--- rawg-server/services/wishlistService.ts
+++ rawg-server/services/wishlistService.ts
@@ -1,15 +1,3 @@
 import { UserCollectionService } from "./userCollectionService";
 
-const wishlistService = new UserCollectionService("wishlist");
-
-export const getUserWithWishlist = async (userId: number) => {
-  return wishlistService.getUserWithCollection(userId);
-};
-
-export const addToWishlist = async (userId: number, gameId: number) => {
-  return wishlistService.addToCollection(userId, gameId);
-};
-
-export const removeFromWishlist = async (userId: number, gameId: number) => {
-  return wishlistService.removeFromCollection(userId, gameId);
-};
+export const wishlistService = new UserCollectionService("wishlist");
