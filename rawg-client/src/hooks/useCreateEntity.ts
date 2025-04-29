409c0064322624b99253903daca53b9eb58cc4c6
Tomas Pesek <pesekt@gmail.com>
2025-04-29 23:56:07 +0200

    updated documentation

--- rawg-client/src/hooks/useCreateEntity.ts
+++ rawg-client/src/hooks/useCreateEntity.ts
@@ -5,6 +5,18 @@ import {
 } from "@tanstack/react-query";
 
 // createFn: The function that creates the entity and returns the created entity or response
+
+/**
+ * useCreateEntity is a generic hook for creating an entity and invalidating the related query.
+ *
+ * @template T - The entity type to create.
+ * @template TResult - The result type returned by the create function (defaults to any).
+ * @param createFn - Function to create the entity (should return a promise of the created entity or response).
+ * @param queryKey - The query key to invalidate after creation (e.g. ["games"]).
+ * @param options - Additional react-query mutation options (e.g. onSuccess, onError, etc).
+ *
+ * @returns A react-query mutation object for the create operation.
+ */
 type CreateFn<T, TResult = any> = (data: Partial<T>) => Promise<TResult>;
 
 /**
