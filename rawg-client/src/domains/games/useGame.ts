292f90eae0366eb1fdf4a68c6aa10fcb6a564176
Tomas Pesek <pesekt@gmail.com>
2025-04-29 23:16:14 +0200

    useGame using the generic hook

--- rawg-client/src/domains/games/useGame.ts
+++ rawg-client/src/domains/games/useGame.ts
@@ -1,10 +1,12 @@
-import { useQuery } from "@tanstack/react-query";
+import useGetEntity from "../../hooks/useGetEntity";
+import { Game } from "./Game";
 import gameService from "./gameService";
 
-const useGame = (id: number) =>
-  useQuery({
-    queryKey: ["games", id],
-    queryFn: () => gameService.get(id),
+const useGame = (id?: number | null) =>
+  useGetEntity<Game>({
+    idOrSlug: id,
+    queryKeyPrefix: "game",
+    service: gameService,
   });
 
 export default useGame;
