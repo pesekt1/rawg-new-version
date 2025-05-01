ab5ded5f56ba3a687f00f6203e26abbdf8bcaa92
Tomas Pesek <pesekt@gmail.com>
2025-05-01 12:43:05 +0200

    auto scroll to the top when navigate

--- rawg-client/src/pages/Layout.tsx
+++ rawg-client/src/pages/Layout.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "../components/NavBar/NavBar";
 import { Box } from "@chakra-ui/react";
+import ScrollToTop from "../components/ScrollToTop";
 
 const Layout = () => {
   return (
     <Box mt={5}>
+      <ScrollToTop />
       <NavBar />
       <Box padding={5}>
         <Outlet />
