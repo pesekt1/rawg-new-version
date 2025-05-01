91194edbafa50e0474c4685c767236d26a13559b
Tomas Pesek <pesekt@gmail.com>
2025-05-01 13:27:31 +0200

    refactored - now using centralized errorHandler

--- rawg-server/routes/GameRouter.ts
+++ rawg-server/routes/GameRouter.ts
@@ -9,78 +9,74 @@ import {
   updateGame,
 } from "../services/gameService";
 import { requireAdmin } from "../middleware/requireAdmin";
+import { asyncHandler } from "../utils/asyncHandler";
 
 const gameRouter = Router();
 
-gameRouter.get("/", async (req, res) => {
-  try {
-    //getGames returns the modified object to with the client
+gameRouter.get(
+  "/",
+  asyncHandler(async (req, res) => {
     const response = await getGames(req);
     res.send(response);
-  } catch (error) {
-    res.status(500).send({ error: "An unexpected error occurred." });
-  }
-});
+  })
+);
 
 // Protect create, update, delete routes
-gameRouter.post("/", requireAdmin, async (req, res) => {
-  try {
+gameRouter.post(
+  "/",
+  requireAdmin,
+  asyncHandler(async (req, res) => {
     const game = await createGame(req.body);
     res.status(201).send(game);
-  } catch (error) {
-    res.status(400).send({ error: (error as Error).message });
-  }
-});
+  })
+);
 
 // Change from :slug to :id and parse as number
-gameRouter.get("/:id", async (req, res) => {
-  try {
+gameRouter.get(
+  "/:id",
+  asyncHandler(async (req, res) => {
     const id = parseInt(req.params.id, 10);
     const game = await getGame(id);
     res.send(game);
-  } catch (error) {
-    res.status(500).send({ error: "An unexpected error occurred." });
-  }
-});
+  })
+);
 
-gameRouter.get("/:id/movies", async (req, res) => {
-  try {
+gameRouter.get(
+  "/:id/movies",
+  asyncHandler(async (req, res) => {
     const gameId = parseInt(req.params.id, 10);
     const trailers = await getTrailers(gameId);
     res.send(trailers);
-  } catch (error) {
-    res.status(500).send({ error: "An unexpected error occurred." });
-  }
-});
+  })
+);
 
-gameRouter.get("/:id/screenshots", async (req, res) => {
-  try {
+gameRouter.get(
+  "/:id/screenshots",
+  asyncHandler(async (req, res) => {
     const gameId = parseInt(req.params.id, 10);
     const screenshots = await getScreenshots(gameId);
     res.send(screenshots);
-  } catch (error) {
-    res.status(500).send({ error: "An unexpected error occurred." });
-  }
-});
+  })
+);
 
-gameRouter.delete("/:id", requireAdmin, async (req, res) => {
-  try {
+gameRouter.delete(
+  "/:id",
+  requireAdmin,
+  asyncHandler(async (req, res) => {
     const id = parseInt(req.params.id, 10);
     await deleteGame(id);
     res.status(204).send();
-  } catch (error) {
-    res.status(404).send({ error: (error as Error).message });
-  }
-});
+  })
+);
 
-gameRouter.patch("/:id", requireAdmin, async (req, res) => {
-  try {
+gameRouter.patch(
+  "/:id",
+  requireAdmin,
+  asyncHandler(async (req, res) => {
     const id = parseInt(req.params.id, 10);
     const updatedGame = await updateGame(id, req.body);
     res.send(updatedGame);
-  } catch (error) {
-    res.status(400).send({ error: (error as Error).message });
-  }
-});
+  })
+);
 
 export default gameRouter;
--- rawg-server/routes/authRouter.ts
+++ rawg-server/routes/authRouter.ts
@@ -1,26 +1,25 @@
 import { Router } from "express";
 import { AuthService } from "../services/authService";
+import { asyncHandler } from "../utils/asyncHandler";
 
 const authRouter = Router();
 
-authRouter.post("/register", async (req, res) => {
-  try {
+authRouter.post(
+  "/register",
+  asyncHandler(async (req, res) => {
     const { username, password } = req.body;
     const user = await AuthService.register(username, password);
     res.status(201).send(user);
-  } catch (error) {
-    res.status(400).send({ error: (error as Error).message });
-  }
-});
+  })
+);
 
-authRouter.post("/login", async (req, res) => {
-  try {
+authRouter.post(
+  "/login",
+  asyncHandler(async (req, res) => {
     const { username, password } = req.body;
     const result = await AuthService.login(username, password);
     res.send(result);
-  } catch (error) {
-    res.status(400).send({ error: (error as Error).message });
-  }
-});
+  })
+);
 
 export default authRouter;
