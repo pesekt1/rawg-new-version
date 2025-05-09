4a0eecc050a673f8e1589ea9579b35c989c60217
pesekt1 <pesekt@gmail.com>
2025-05-09 14:24:06 +0200

    added a comment

--- rawg-server/controllers/HelloController.ts
+++ rawg-server/controllers/HelloController.ts
@@ -1,5 +1,8 @@
 import { Controller, Get, Route } from "tsoa";
 
+// This controller is only for a basic "hello world" test endpoint.
+// If you do not need a test or demo endpoint, you can safely delete this file and its route.
+
 @Route("hello")
 export class HelloController extends Controller {
   @Get("/")
