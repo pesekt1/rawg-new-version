64696faf5884d3266b947d23f76c45be4a191a75
pesekt1 <pesekt@gmail.com>
2025-05-12 19:58:19 +0200

    created expand-collapse button

---
+++ rawg-client/src/components/ExpandCollapseButton.tsx
@@ -0,0 +1,24 @@
+import { Button } from "@chakra-ui/react";
+import { FiChevronUp, FiChevronDown } from "react-icons/fi";
+
+interface ExpandCollapseButtonProps {
+  isExpanded: boolean;
+  onToggle: () => void;
+  iconSize?: string | number;
+}
+
+const ExpandCollapseButton = ({
+  isExpanded,
+  onToggle,
+  iconSize = 20,
+}: ExpandCollapseButtonProps) => (
+  <Button variant="expandCollapse" onClick={onToggle}>
+    {isExpanded ? (
+      <FiChevronUp size={iconSize} />
+    ) : (
+      <FiChevronDown size={iconSize} />
+    )}
+  </Button>
+);
+
+export default ExpandCollapseButton;
