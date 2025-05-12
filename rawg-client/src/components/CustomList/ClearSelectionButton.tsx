bacc29d593e9c375e634c4a4bf2db3dd29f43878
pesekt1 <pesekt@gmail.com>
2025-05-12 20:37:26 +0200

    created clear selection button

---
+++ rawg-client/src/components/CustomList/ClearSelectionButton.tsx
@@ -0,0 +1,20 @@
+import { Button } from "@chakra-ui/react";
+import { FiX } from "react-icons/fi";
+
+interface ClearSelectionButtonProps {
+  onClear: () => void;
+}
+
+const ClearSelectionButton = ({ onClear }: ClearSelectionButtonProps) => (
+  <Button
+    variant="customButton"
+    onClick={onClear}
+    padding={0}
+    height="auto"
+    minWidth={0}
+  >
+    <FiX size={16} />
+  </Button>
+);
+
+export default ClearSelectionButton;
