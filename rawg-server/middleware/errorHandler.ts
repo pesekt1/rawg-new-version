1012044781b22c91d7710c65ac2160ffc3392bc7
pesekt1 <pesekt@gmail.com>
2025-05-12 12:19:41 +0200

    fixed errorHandler to use the custom message if provided

--- rawg-server/middleware/errorHandler.ts
+++ rawg-server/middleware/errorHandler.ts
@@ -12,6 +12,6 @@ export function errorHandler(
   next: NextFunction
 ) {
   const status = err.status || 500;
-  const message = err.message || "Internal server error";
+  const message = status === 500 ? "Internal server error" : err.message;
   res.status(status).json({ error: message });
 }
