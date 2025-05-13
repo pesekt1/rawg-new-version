90e5d59c61e1fc0cbb558452cffd907c94096ec6
pesekt1 <pesekt@gmail.com>
2025-05-13 19:13:52 +0200

    fixed a bug with useColorMode in ListItemButton

--- rawg-client/src/components/CustomList/ListItemButton.tsx
+++ rawg-client/src/components/CustomList/ListItemButton.tsx
@@ -1,5 +1,4 @@
 import { Button, useColorMode } from "@chakra-ui/react";
-const { colorMode } = useColorMode();
 
 interface ListItemButtonProps {
   text: string;
@@ -7,21 +6,24 @@ interface ListItemButtonProps {
   onClick: () => void;
 }
 
-const colorSelected = colorMode === "light" ? "accent.700" : "yellow.300";
+const ListItemButton = ({ text, isSelected, onClick }: ListItemButtonProps) => {
+  const { colorMode } = useColorMode(); // Moved inside the component
+  const colorSelected = colorMode === "light" ? "accent.700" : "yellow.300";
 
-const ListItemButton = ({ text, isSelected, onClick }: ListItemButtonProps) => (
-  <Button
-    variant="customButton"
-    color={isSelected ? colorSelected : undefined}
-    p={1}
-    textAlign="left"
-    whiteSpace="normal"
-    fontSize="sm"
-    _focus={{ boxShadow: "none" }} // Updated to a valid SystemStyleObject
-    onClick={onClick}
-  >
-    {text}
-  </Button>
-);
+  return (
+    <Button
+      variant="customButton"
+      color={isSelected ? colorSelected : undefined}
+      p={1}
+      textAlign="left"
+      whiteSpace="normal"
+      fontSize="sm"
+      _focus={{ boxShadow: "none" }}
+      onClick={onClick}
+    >
+      {text}
+    </Button>
+  );
+};
 
 export default ListItemButton;
