2ccf9cfaddaf95fee2b8196f299a0c976302b4bb
pesekt1 <pesekt@gmail.com>
2025-05-17 00:47:05 +0200

    added dtos for screenshot and trailer

---
+++ rawg-server/controllers/dto/ScreenshotReadDto.ts
@@ -0,0 +1,7 @@
+export interface ScreenshotReadDto {
+  id: number;
+  image: string;
+  width: number;
+  height: number;
+  is_deleted: boolean;
+}
---
+++ rawg-server/controllers/dto/TrailerReadDto.ts
@@ -0,0 +1,7 @@
+export interface TrailerReadDto {
+  id: number;
+  name: string;
+  preview: string;
+  data480: string;
+  dataMax: string;
+}
