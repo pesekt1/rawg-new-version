77511500ad06f6c8b4404e63a05ca80d6aa00640
pesekt1 <pesekt@gmail.com>
2025-05-17 23:26:17 +0200

    updated user avatar design

--- rawg-client/src/domains/user/UserAvatar.tsx
+++ rawg-client/src/domains/user/UserAvatar.tsx
@@ -1,30 +1,32 @@
-import { Avatar, AvatarProps } from "@chakra-ui/react";
+import { Avatar, AvatarProps, useColorModeValue } from "@chakra-ui/react";
 
 interface UserAvatarProps extends AvatarProps {
   username: string;
 }
 
 const UserAvatar = ({ username, size = "md", ...props }: UserAvatarProps) => {
+  const bg = useColorModeValue("white", "gray.700");
+  const color = useColorModeValue("black", "white");
+  const hoverBg = useColorModeValue("teal.100", "teal.700");
+  const activeBg = useColorModeValue("teal.200", "teal.800");
+
   return (
     <Avatar
       name={username}
       size={size}
       cursor="pointer"
-      bg="gray.200"
-      _dark={{ bg: "gray.700" }}
-      color="white"
+      bg={bg}
+      color={color}
       fontSize={size === "sm" ? "sm" : "md"}
       showBorder
       borderColor="teal.300"
       _hover={{
         boxShadow: "0 0 0 2px #319795",
-        bg: "teal.100",
-        _dark: { bg: "teal.700" },
+        bg: hoverBg,
       }}
       _active={{
         boxShadow: "0 0 0 2px #319795",
-        bg: "teal.200",
-        _dark: { bg: "teal.800" },
+        bg: activeBg,
       }}
       {...props} // Spread additional props for flexibility
     />
