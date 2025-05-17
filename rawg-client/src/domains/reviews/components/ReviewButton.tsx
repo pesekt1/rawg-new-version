43b4c822fbb63edfd49fc78d4fa32d6834166db5
pesekt1 <pesekt@gmail.com>
2025-05-17 18:51:47 +0200

    extracted ReviewButton from GameDetailPage.

---
+++ rawg-client/src/domains/reviews/components/ReviewButton.tsx
@@ -0,0 +1,25 @@
+import { Button } from "@chakra-ui/react";
+import { FaPlus } from "react-icons/fa";
+
+const ReviewButton = ({
+  userReview,
+  role,
+  onOpenReviewModal,
+}: {
+  userReview: any;
+  role: string | null;
+  onOpenReviewModal: () => void;
+}) => (
+  <Button
+    marginTop={2}
+    variant="outlinedButton"
+    size="sm"
+    onClick={onOpenReviewModal} // Trigger the modal
+    isDisabled={!role} // Disable the button if the user is not authenticated
+    leftIcon={<FaPlus />} // Add the Plus icon to the button
+  >
+    {userReview ? "Update Review" : "Create Review"}
+  </Button>
+);
+
+export default ReviewButton;
