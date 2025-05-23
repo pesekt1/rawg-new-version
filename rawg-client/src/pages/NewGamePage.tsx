ffb3c170dab31d2e05d058bf1f7566ea237418bb
pesekt1 <pesekt@gmail.com>
2025-05-23 22:41:07 +0200

    updated the game pages to simplify and avoid duplication

--- rawg-client/src/pages/NewGamePage.tsx
+++ rawg-client/src/pages/NewGamePage.tsx
@@ -1,24 +1,13 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import useCreateGame from "../domains/games/useCreateGame";
-import useGenres from "../domains/genres/useGenres";
-import usePublishers from "../domains/publishers/usePublishers";
 import { useAuth } from "../domains/auth/useAuth";
-import useStores from "../domains/stores/useStores";
-import usePlatforms from "../domains/platforms/usePlatforms";
 import GameForm from "../domains/games/components/GameForm/GameForm";
-import useDevelopers from "../domains/developers/useDevelopers";
-import useTags from "../domains/tags/useTags";
 
 const NewGamePage = () => {
   const { mutate, isLoading, isSuccess, isError, error, reset } =
     useCreateGame();
-  const genres = useGenres();
-  const platforms = usePlatforms();
-  const stores = useStores();
-  const publishers = usePublishers();
-  const developers = useDevelopers();
-  const tags = useTags();
+
   const { isAuthenticated } = useAuth();
   const navigate = useNavigate();
   useEffect(() => {
@@ -43,12 +32,6 @@ const NewGamePage = () => {
         developers: [],
         tags: [],
       }}
-      genresData={genres.data}
-      platformsData={platforms.data}
-      storesData={stores.data}
-      publishersData={publishers.data}
-      developersData={developers.data}
-      tagsData={tags.data}
       onSubmit={(values) => {
         reset();
         mutate(values, {
