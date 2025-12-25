28d1545989c331ab652574b612f0fb1bf39ec531
pesekt1 <pesekt@gmail.com>
2025-12-25 20:11:40 +0100

    truncate username if too long in the UserCard component

--- rawg-client/src/domains/user/UserCard.tsx
+++ rawg-client/src/domains/user/UserCard.tsx
@@ -1,18 +1,28 @@
-import { Card, CardBody, Text } from "@chakra-ui/react";
-import UserAvatar from "./UserAvatar";
+import { Box, Card, CardBody, Text } from "@chakra-ui/react";
 import { User } from "../../interfaces/User";
+import UserAvatar from "./UserAvatar";
 
 interface UserCardProps {
   user: User;
 }
 
 const UserCard = ({ user }: UserCardProps) => (
-  <Card>
-    <CardBody display="flex" alignItems="center" gap={3}>
-      <Text fontWeight="bold" fontSize="2xl">
-        {user.username}
-      </Text>
-      <UserAvatar user={user} size="md" />
+  <Card overflow="hidden">
+    <CardBody
+      display="flex"
+      alignItems="center"
+      justifyContent="space-between"
+      gap={3}
+    >
+      <Box flex="1" minW={0}>
+        <Text fontWeight="bold" fontSize="2xl" isTruncated>
+          {user.username}
+        </Text>
+      </Box>
+
+      <Box flexShrink={0}>
+        <UserAvatar user={user} size="md" />
+      </Box>
     </CardBody>
   </Card>
 );
