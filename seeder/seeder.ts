ddce337c8a1043d1a1682dd2b9510972bfefb598
pesekt1 <pesekt@gmail.com>
2025-12-25 17:55:44 +0100

    added comments to the seeder

--- seeder/seeder.ts
+++ seeder/seeder.ts
@@ -2,17 +2,18 @@ import { config } from "dotenv"; // Import dotenv
 config(); // Load environment variables
 
 import { AppDataSource } from "./data-source";
-import { truncateAllTables } from "./utils/truncateAllTables";
-import { seedUser } from "./utils/seedUser";
 import { seedGames } from "./utils/seedGames";
+import { seedUser } from "./utils/seedUser";
+import { truncateAllTables } from "./utils/truncateAllTables";
 
 //await cannot be used at the top level in a module, so we wrap it in an async IIFE
 (async () => {
   await AppDataSource.initialize(); //initialize connection
 
-  const mode = process.argv[2];
+  const mode = process.argv[2]; // Get mode from command line arguments
   const gamePages = parseInt(process.env.GAME_PAGES || "1", 10); // Default to 1 if not set
 
+  // If mode is "user", only seed users
   if (mode === "user") {
     await seedUser(AppDataSource);
     console.log("User seeding completed successfully.");
