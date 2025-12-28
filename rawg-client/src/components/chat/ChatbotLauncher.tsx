24be5283baaf180a4b3ea1355ee8e9abd6f58875
pesekt1 <pesekt@gmail.com>
2025-12-28 21:58:37 +0100

    updated chatbot launcher button style

--- rawg-client/src/components/chat/ChatbotLauncher.tsx
+++ rawg-client/src/components/chat/ChatbotLauncher.tsx
@@ -1,4 +1,5 @@
-import { IconButton, Portal, useDisclosure } from "@chakra-ui/react";
+import { IconButton, Portal, Tooltip, useDisclosure } from "@chakra-ui/react";
+import { FaRobot } from "react-icons/fa";
 import { ChatbotModal } from "./ChatbotModal";
 
 export function ChatbotLauncher() {
@@ -7,18 +8,23 @@ export function ChatbotLauncher() {
   return (
     <>
       <Portal>
-        <IconButton
-          aria-label="Open chat"
-          onClick={onOpen}
-          position="fixed"
-          right={4}
-          bottom={4}
-          zIndex={99999}
-          colorScheme="blue"
-          borderRadius="full"
-          size="lg"
-          icon={<span>Chat</span>}
-        />
+        <Tooltip label="Chat with us" placement="left" hasArrow>
+          <IconButton
+            aria-label="Open chat"
+            onClick={onOpen}
+            position="fixed"
+            right={4}
+            bottom={4}
+            zIndex={99999}
+            colorScheme="teal"
+            borderRadius="full"
+            border="2px solid black"
+            size="lg"
+            fontSize="3xl"
+            boxShadow="lg"
+            icon={<FaRobot />}
+          />
+        </Tooltip>
       </Portal>
 
       <ChatbotModal isOpen={isOpen} onClose={onClose} />
